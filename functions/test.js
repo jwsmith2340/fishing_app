f86a6b6fb062c4f7e066f69ad9a9f6870ffb6c33
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-04-28 19:58:27 -0400

    Commented in the docs and some explanations.

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,7 @@
 const db = require('../database/databasepg');
 
-//##########################################################
+// This page isn't being exported, it is just here to serve to show examples of common query and insert/ update formats
+// used natively by node-postgres. The full documentation for this page: https://node-postgres.com/features/queries
 
 // Select statements to query from the DB:
 
@@ -18,7 +19,11 @@ db.query(`SELECT * FROM TEST`).then(res => console.log(res.rows[0])).catch(e => 
 
 // ###########################################################
 
-// Parametrized query
+// Parametrized queries
+
+// in these examples, 'text' is the query, and 'values' are the values in an array: 
+let text = `INSERT INTO TEST (name, user_statement) VALUES ($1, $2)`;
+let values = ['A name', 'Some type of statement'];
 
 // Callback function
 db.query(text, values, (err, res) => {
