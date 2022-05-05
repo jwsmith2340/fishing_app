e598488bd46a30649ce9009778ec4c0dedb65599
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-05-04 20:48:13 -0400

    Added cors dependency.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const pool = require('./database/databasepg');
 const std = require('std')
+const cors = require("cors")
 const trip_router = require('./controllers/trip')
 const fly_box_router = require('./controllers/fly-box');
 const river_router = require('./controllers/river');
@@ -9,12 +10,14 @@ const app = express()
 
 const port = 4000
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('good send')
 })
 
-app.use('/trip', trip_router)
-app.use('/fly_box', fly_box_router)
-app.use('/river', river_router)
+app.use(cors());
+
+app.use('/api/trip', trip_router)
+app.use('/api/fly_box', fly_box_router)
+app.use('/api/river', river_router)
 
 app.listen(port, () => console.log(`listening on ${port}`));
