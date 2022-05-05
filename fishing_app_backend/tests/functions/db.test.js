45f2128461c1c244b565c00bc12776ef47697ee1
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-05-04 20:48:35 -0400

    Clearing up git mess fro mdeleting file.

--- fishing_app_backend/tests/functions/db.test.js
+++
@@ -1,8 +0,0 @@
-//  /functions/db.js test
-const db = require('../../functions/db')
-
-test('Testing db.recs function', () => {
-
-    expect(db.recs(`SELECT TEST_ID FROM TEST WHERE TEST_ID = 1`)).toBe([[{test_id: 1}]])
-
-});
