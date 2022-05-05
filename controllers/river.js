b90a9cd3a5307dd960b2cdb9d08b74a08e68f040
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-05-04 20:46:18 -0400

    Set up asynchronous route to retrieve basic DB call.

--- controllers/river.js
+++ controllers/river.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const db = require('../functions/db')
 const river_router = express.Router();
 
-river_router.get('/', (req, res) => {
 
-    res.send('river page');
+river_router.get('/', async (req, res) => {
+
+    try {
+        res.send(await db.recs(`SELECT * FROM FLY`))
+    } catch (error) {
+        res.status(400).json(error);
+    }
+
 
 });
 
