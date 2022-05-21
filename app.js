76fd2c3db5c450b2c46b1e4786299d9596c654c0
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-05-21 15:37:02 -0400

    Updated routes.

--- app.js
+++ app.js
@@ -5,19 +5,17 @@ const cors = require("cors")
 const trip_router = require('./controllers/trip')
 const fly_box_router = require('./controllers/fly-box');
 const river_router = require('./controllers/river');
+const index_router = require('./controllers/index')
 
 const app = express()
 
 const port = 4000
 
-app.get('/api', (req, res) => {
-    res.send('good send')
-})
-
 app.use(cors());
 
-app.use('/api/trip', trip_router)
-app.use('/api/fly_box', fly_box_router)
-app.use('/api/rivers', river_router)
+app.use('/api', index_router);
+app.use('/api/trip', trip_router);
+app.use('/api/fly_box', fly_box_router);
+app.use('/api/rivers', river_router);
 
 app.listen(port, () => console.log(`listening on ${port}`));
